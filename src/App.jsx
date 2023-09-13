14897786b9de837ca99436f5296e91811d7711d0
Lolicon <138757081+Lolicon0@users.noreply.github.com>
2023-09-13 17:04:11 +0700

    reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,18 @@
-import React, { useState } from "react";
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const Chat = () => {
-  const [messages, setMessages] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const message = event.target.elements.message.value;
-    setMessages([...messages, message]);
-  };
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h3>Chat</h3>
-      <ul>
-        {messages.map((message) => (
-          <li key={message}>{message}</li>
-        ))}
-      </ul>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="message" />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-  );
-};
-
-const Menu = () => {
-  return (
-    <nav>
-      <ul>
-        <li><a href="#">Home | </a>
-        <a href="#">About</a>
-        <a href="#"> | Contact</a></li>
-      </ul>
-    </nav>
-  );
-};
-
-const App = () => {
-  return (
-    <div>
-      <header>
-        <img src="https://static.thairath.co.th/media/Dtbezn3nNUxytg04anFWUDVYRmYXNGatR95AMXpphpB9AW.webp" width="300" height="200" />
-      <div style={{display: "inline-block"}}>
-        <Menu />
+    <>
+      <div>
+          
       </div>
-      </header>
-      <Chat />
-    </div>
-  );
-};
+    </>
+  )
+}
 
-
-export default App;
+export default App
