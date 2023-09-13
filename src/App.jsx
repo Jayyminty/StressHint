e568a7a0d4c3e5f8e19b0a0c1dedb4daf8a21c1e
Jayyminty <jedaijr7@gmail.com>
2023-09-13 17:19:53 +0700

    navbar 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-          
-      </div>
+    <div className='navbar'>
+      <p>
+        sfdhdrfghjk
+      </p>
+      <p>
+        sfdhdrfghjk
+      </p>
+    </div>
     </>
   )
 }
