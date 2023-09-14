1045804b89f2bd7a9b01de94baf5f7c8a5c886a2
Lolicon <138757081+Lolicon0@users.noreply.github.com>
2023-09-14 12:57:09 +0700

    add logopicture

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useRef } from 'react';
+import { Button, Navbar, NavLink } from 'reactstrap';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
+  const countRef = useRef(0);
 
   return (
     <>
-    <div className='navbar'>
-      <p>
-        sfdhdrfghjk
-      </p>
-      <p>
-        sfdhdrfghjk
-      </p>
-    </div>
-    </>
-  )
-}
+    <div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+          <img src="/Image/logomain.jpg.jpg" width={50}></img>
+      </a>
 
-export default App
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+      </ul>
+    </header>
+  </div>
+    </>
+  );
+};
+
+export default App;
